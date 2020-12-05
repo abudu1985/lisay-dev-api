9bbf042a2a94ab8d15eb9fd549860d373947c12b
igor.l <luchkoigor85@gmail.com>
2020-12-05 22:55:43 +0200

    add cors

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require('cors');
 
 const users = require('./routes/api/users');
 const quotes = require('./routes/api/quotes');
 
 const app = express();
+app.use(cors());
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
